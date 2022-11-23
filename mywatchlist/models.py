b5684238307314fd7a5a1a523cd35f518eda508d
ihzadfm <ihzadfm@gmail.com>
2022-11-24 00:54:42 +0700

    bismillah9

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class MyWatchList(models.Model):
-    is_watched = models.TextField()
+    watched = models.BooleanField(default=False)
     title = models.CharField(max_length=255)
-    rating = models.FloatField()
-    release_date = models.TextField()
+    rating = models.IntegerField()
+    release_date = models.DateField()
     review = models.TextField()
